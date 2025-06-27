85a5a9d1388093ecc7b9abeea590938a30b99d16
Jerryomwesh <jerryomwesh@gmail.com>
2025-06-27 16:01:57 +0300

    updates js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+// gobal variable customerName using var
+var customerName = 'bob';
+
+// Function to uppercase the global customerName variable
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// Function to declare a global variable bestCustomer and assign it 'not bob'
+function setBestCustomer() {
+    bestCustomer = 'not bob'; // Implicit global variable declaration
+}
+
+// Function to overwrite the bestCustomer variable
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+// Declare a constant leastFavoriteCustomer in global scope
+const leastFavoriteCustomer = 'someone';
+
+// Function attempting to change the constant leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'someone else'; 
+}
+
+
+
